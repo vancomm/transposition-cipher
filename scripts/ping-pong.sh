0cd4fdd83903e24ec80f741d5fe41255d80de1a7
vancomm <peshekhonov.ip@gmail.com>
2023-10-21 16:51:15 +0300

    Tweak script

--- scripts/ping-pong.sh
+++ scripts/ping-pong.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 
 message="СВЯЩЕННАЯ РИМСКАЯ ИМПЕРИЯ"
-size=10
+size="${1:=4}"
 
 command time -f "Encoding time: %es" \
     t_cipher encode "$message" \
         --key-size "$size" \
-        --key-seed 0 \
-        --no-print-key |\
+        --key-seed 0 |\
     tee /dev/tty |\
     xargs -I {} time -f "Decoding time: %es" \
         t_cipher decode "{}" \
